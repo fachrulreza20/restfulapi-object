9baef9ebdd09d2250be5f313156b0369a8acc835
fachrulreza20@gmai.com <fachrulreza20@gmail.com>
2021-11-01 13:29:05 +0700

    create project

---
+++ config/Database.php
@@ -0,0 +1,20 @@
+<?php
+    class Database{
+    //pembuatan variabel
+    private $host = "localhost"; 
+    private $db_name = "restfullapi-object";
+    private $username ="root"; 
+    private $password ="";
+    public $conn;
+    //get the data base connection 
+    public function getConnection(){
+        $this->conn = null;
+        try{
+        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        $this->conn->exec("set names utf8");
+    }catch(PDOException $exception){
+        echo "Connectionerror:" . $exception->getMessage();
+    }return $this->conn;
+    }
+}
+?>
